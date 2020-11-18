9dd97263ef659ac2e13589a627a5a1c0e0635d58
heehee3 <52603058+heehee3@users.noreply.github.com>
2020-11-18 18:31:16 -0500

    Add files via upload

---
+++ validar.php
@@ -0,0 +1,21 @@
+<?php
+
+    $usuario=$_POST['Usuario'];
+    $contrasena=$_POST['Contraseña'];
+
+    $conexion= mysqli_connect("localhost","root","","bdproyecto") or die('Error en la conexion');
+    
+    $consulta="SELECT * FROM usuario WHERE NOMBREUSUARIO='$usuario' AND CONTRASENAUSUARIO='$contrasena'";
+
+    $resultado=mysqli_query($conexion,$consulta) or die('Error de query');
+
+    $filas=mysqli_num_rows($resultado);
+    if($filas>0){
+        header("location:menuprincipal.html");
+    }else {
+        header("location:datosincorrectos.html");
+    }
+    
+    mysqli_free_result($resultado);
+    mysqli_close($conexion);
+?>
